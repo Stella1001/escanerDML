d0d233fe2ab549c789ecdf21f0c3598f44e7a682
stella <stellaperez613@gmail.com>
2025-03-02 21:24:59 -0700

    fixing css

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
+
+
+
+
 // Inicializamos variables de texto, tablas léxica y de símbolos, y arreglo de errores.
 $texto = "";
 $lexical_table = array();
@@ -183,7 +187,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["regexInput"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Escáner DML</title>
-    <link rel="stylesheet" href="api/style.css">
+    <link rel="stylesheet" href="<?php echo '/styles.css'; ?>">
+
     <script>
         function resetForm() {
             document.getElementById("regexInput").value = "";
